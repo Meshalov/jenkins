04cd947b773baeb5afc979322f8a3ffb1e125a0d
Meshalov <avmeshalov@gmail.com>
2023-01-16 18:10:32 +0300

    01/16/2023 18:10:32

--- PushGit.ps1
+++
@@ -1,4 +0,0 @@
-﻿$date = Get-Date
-cd C:\Users\Aleksandr.HOST\Documents\GitHub\jenkins | git add .
-cd C:\Users\Aleksandr.HOST\Documents\GitHub\jenkins | git commit -m "$date"
-cd C:\Users\Aleksandr.HOST\Documents\GitHub\jenkins | git push origin
